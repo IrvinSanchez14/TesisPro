548e023b987748a57d0bfde50f8112bba2c6026a
Irvin Sanchez <irvinsanchez_94@hotmail.com>
2019-02-08 15:59:38 -0600

    fincortez

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const URL = 'http://api.lapizzeria.com/';
+
 class App extends Component {
 
   constructor() {
@@ -9,46 +11,76 @@ class App extends Component {
       total_pos: '',
       total_compras: '',
       fechatiempo: '0000-00-00 00:00:00',
+      ID_cortez: '',
       tasks: []
     }
     this.addTask = this.addTask.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
+
   componentDidMount() {
     this.fetchTasks();
   }
 
   addTask(e) {
-    fetch('http://localhost/api-tesis/cortez/create.php', {
-      method: 'POST',
-      body: JSON.stringify(this.state),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-    .then(data => {
-      M.toast({html: 'task Saved'});
-      this.setState({
-        total_efectivo: '',
-        total_pos: '',
-        total_compras: '',
-      });
-      this.fetchTasks();
-    })
-    .catch(err => console.error(err));
+    if(this.state.ID_cortez) {
+      fetch(`${URL}cortez/update.php`, {
+        method: 'PUT',
+        body: JSON.stringify(this.state),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .then(data => {
+        M.toast({html: 'task Updated'});
+        this.setState({
+          total_efectivo: '',
+          total_pos: '',
+          total_compras: '',
+          ID_cortez: ''
+        });
+        this.fetchTasks();
+      })
+      .catch(err => console.error(err));
 
+    } else {
+      fetch(`${URL}cortez/create.php`, {
+        method: 'POST',
+        body: JSON.stringify(this.state),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .then(data => {
+        M.toast({html: 'task Saved'});
+        this.setState({
+          total_efectivo: '',
+          total_pos: '',
+          total_compras: '',
+        });
+        this.fetchTasks();
+      })
+      .catch(err => console.error(err));
+    }
     e.preventDefault();
   }
 
   fetchTasks() {
-    fetch('http://localhost/api-tesis/cortez/read.php')
+    fetch(`${URL}cortez/read.php`)
       .then(res => res.json())
       .then(data => {
-        this.setState({
-          tasks: data
-        });
+        if(data.ID_cortez == 0) {
+          this.setState({
+            tasks: []
+          });
+        } else {
+          this.setState({
+            tasks: data
+          });
+        }
       });
   }
 
@@ -60,8 +92,8 @@ class App extends Component {
   }
 
   deleteTask(id) {
-    if ( confirm('estas seguro de eliminarlo')) {
-      fetch('http://localhost/api-tesis/cortez/delete.php', {
+    if ( confirm('estas seguro de eliminarlo ?')) {
+      fetch(`${URL}cortez/delete.php`, {
         method: 'DELETE',
         body: JSON.stringify({
           ID_cortez: id
@@ -74,9 +106,25 @@ class App extends Component {
       .then(res => res.json())
       .then(data => {
         M.toast({html: 'task deleted'});
+        console.log(this.state)
         this.fetchTasks();
       });
     }
+
+  }
+
+  editTask(id) {
+    let lista = this.state.tasks
+    lista.filter((list) => 
+      list.ID_cortez === id
+      ).forEach((listCortez) => {
+        this.setState({
+          total_efectivo: listCortez.total_efectivo,
+          total_pos: listCortez.total_pos,
+          total_compras: listCortez.total_compras,
+          ID_cortez: listCortez.ID_cortez
+        })
+      });
 
   }
 
@@ -89,66 +137,70 @@ class App extends Component {
             <a className="brand-logo" href="/">PICNIC CORTEZ</a>
           </div>
         </nav>
-        <div className="container">
+        <div className="container" style={{marginTop: '40px'}}>
           <div className="row">
             <div className="col s5">
               <div className="card">
-                <div className="card-content">
+                <div className="card-content" style={{paddingBottom: '60px'}}>
                   <form onSubmit={this.addTask}>
                     <div className="row">
                       <div className="input field col s12">
-                        <input name="total_efectivo" type="text" placeholder="total Efectivo" onChange={this.handleChange} value={this.state.total_efectivo} />
+                        <label >Total Efectivo</label>
+                          <input name="total_efectivo" type="text" placeholder="Total efectivo" onChange={this.handleChange} value={this.state.total_efectivo} />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input field col s12">
-                        <textarea name="total_pos" onChange={this.handleChange} placeholder="total POS" className="materialize-textarea" value={this.state.total_pos} />
+                        <label>Total POS</label>
+                          <textarea name="total_pos" onChange={this.handleChange} placeholder="Total POS" className="materialize-textarea" value={this.state.total_pos} />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input field col s12">
-                        <textarea name="total_compras" onChange={this.handleChange} placeholder="total_compras" className="materialize-textarea" value={this.state.total_compras} />
+                        <label>Total Compras</label>
+                          <textarea name="total_compras" onChange={this.handleChange} placeholder="Total compras" className="materialize-textarea" value={this.state.total_compras} />
                       </div>
                     </div>
-                    <button className="btn light-blue darken-4" type="submit"> Send</button>
+                    <button className="btn light-blue darken-4" type="submit" style={{float: 'right', width: '100px'}}> Send</button>
                   </form>
                 </div>
               </div>
             </div>
             <div className="col s7">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Total Efectivo</th>
-                    <th>Total_POS</th>
-                    <th>Total_compras</th>
-                    <th>Fecha y Hora</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.tasks.map(task => {
-                      return (
-                        <tr key={task.ID_cortez}>
-                          <td>{`$ ${task.total_efectivo}`}</td>
-                          <td>{`$ ${task.total_pos}`}</td>
-                          <td>{`$ ${task.total_compras}`}</td>
-                          <td>{task.fechatiempo}</td>
-                          <td>
-                            <button className="btn light-blue darken-4" onClick={() => this.editTask(task.ID_cortez)}>
-                              <i className="material-icons">edit</i>
-                              Edit
-                            </button>
-                            <button className="btn light-blue darken-4" style={{margin: '2px'}} onClick={() => this.deleteTask(task.ID_cortez)}>
-                            <i className="material-icons">delete</i>
-                            </button>
-                          </td>
-                        </tr>
-                      )
-                    })
-                  }
-                </tbody>
-              </table>
+              <div className="card">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Total Efectivo</th>
+                      <th>Total POS</th>
+                      <th>Total compras</th>
+                      <th>Fecha y Hora</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      this.state.tasks.map(task => {
+                        return (
+                          <tr key={task.ID_cortez}>
+                            <td>{`$ ${task.total_efectivo}`}</td>
+                            <td>{`$ ${task.total_pos}`}</td>
+                            <td>{`$ ${task.total_compras}`}</td>
+                            <td>{task.fechatiempo}</td>
+                            <td>
+                              <button className="btn light-blue darken-4" style={{marginRight: '4px', marginBottom: '4px'}} onClick={() => this.editTask(task.ID_cortez)}>
+                                <i className="material-icons">edit</i>
+                              </button>
+                              <button className="btn light-blue darken-4" style={{marginBottom: '4px'}} onClick={() => this.deleteTask(task.ID_cortez)}>
+                              <i className="material-icons">delete</i>
+                              </button>
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+                    </tbody>
+                  </table>
+                </div>
             </div>
           </div>
         </div>
